bf1a34334b702485819cce2c6a5ce752ab05bbd3
00Josexyz00 <jose.cortez@epn.edu.ec>
2019-04-30 10:22:12 -0500

    Deber

--- ejercicio3/index.php
+++ ejercicio3/index.php
@@ -1,4 +1,4 @@
-<?php
+<!--
 /**
  * Crear un arreglo asociativo que almacene las claves de acceso de 5 usuarios de un sistema. Acceder a cada componente por su nombre.
  El arreglo debe tener el formato
@@ -10,5 +10,50 @@
  Si los datos no son correctos imprimir un mensaje de error.
  Si los datos son correctos debe redirigir a otra página bienvenido.php que debe imprimir el mensaje "Bienvendo JUAN". (con el nombre de usuario en mayúsculas)
  */
+-->
 
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title> Ejercicio 3 Formulario</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<form>
+  <div class="form-group">
+    <label for="nombre">Nombre</label>
+    <input type="text" class="form-control" id="nombre">
+  </div>
+  <div class="form-group">
+    <label for="pass">Password</label>
+    <input type="password" class="form-control" id="pass" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-primary">Guardar</button>
+</form>
+
+
+
+
+	<?php 
+
+		$arreglo= array('Usuario 1:'=>array('Nombre' => "Jose",'contraseña'=>'jose'),
+						'Usuario 2:'=>array	('Nombre' => "Josselyn",'contraseña'=>'joss'),
+						'Usuario 3:'=>array	('Nombre' => "Edy",'contraseña'=>'edy'));
+
+
+		foreach($arreglo as $usuario => $info){
+			echo '<strong>'.$usuario.'</strong><br>';
+			foreach($info as $content){
+				echo $content.'<br>';
+			}
+
+		}
+
+		
+		
+	 ?>
+	
+</body>
+</html>
